dc31e3776413d3eb07e1a242d7f753b511c989f2
nicolás <usuario@nicolas.fibertel.com.ar>
2026-03-19 18:18:43 -0300

    prototipo ej. 1

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,12 +1,18 @@
 import scala.io.Source
 
 object FileIO {
-  // Pure function to read subscriptions from a JSON file
-  def readSubscriptions(): List[String] = {
-    List(
-      "https://www.reddit.com/r/scala/.json?count=10",
-      "https://www.reddit.com/r/learnprogramming/.json?count=10"
-    )
+  /* Pure function to read subscriptions from a JSON file */
+  def readSubscriptions(): List[Main.Subscription] = {
+    val source = Source.fromFile("./subscriptions.json")
+    val lines = source.getLines().toList
+    val filtered_lines = lines.filter(line => line.contains("name") || line.contains("url"))
+    source.close()
+    val output = filtered_lines.grouped(2).map { line =>
+      val name = line(0).split(":", 2)(1).trim.stripPrefix("\"").stripSuffix("\",")
+      val url = line(1).split(":", 2)(1).trim.stripPrefix("\"").stripSuffix("\"")
+      (name, url)
+    }.toList
+    output
   }
 
   // Pure function to download JSON feed from a URL
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,14 @@
 object Main {
+  type Subscription = (String, String)
+  type Post = (String, String, String, String)
   def main(args: Array[String]): Unit = {
-    val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    val subscriptions: List[Subscription] = FileIO.readSubscriptions()
 
-    val allPosts: List[(String, String)] = subscriptions.map { url =>
-      println(s"Fetching posts from: $url")
-      val posts = FileIO.downloadFeed(url)
-      (url, posts)
+    val allPosts: List[Subscription] = subscriptions.map { subscription =>
+      println(s"Fetching posts from: ${subscription._2}")
+      val posts = FileIO.downloadFeed(subscription._2)
+      (subscription._2, posts)
     }
 
     val output = allPosts
