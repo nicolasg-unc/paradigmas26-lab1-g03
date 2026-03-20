70916b8f74681791bf6166cf869766d940c3afa9
Alejo Vides <alejo.vides@mi.unc.edu.ar>
2026-03-20 17:06:49 -0300

    ejercicio 2

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,14 +1,17 @@
 import scala.io.Source
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 object FileIO {
   /* Pure function to read subscriptions from a JSON file */
   def readSubscriptions(): List[Main.Subscription] = {
     implicit val formats = org.json4s.DefaultFormats
     val source = Source.fromFile("./subscriptions.json")
-    val stringSource = source.mkString
-    val json = parse(stringSource).children.map { item =>
+    val content = source.mkString
+    val json = parse(content).children.map { item =>
       val url = (item \ "url").extract[String]
       val name = (item \ "name").extract[String]
       (name, url)
@@ -17,9 +20,35 @@ object FileIO {
     json
   }
 
+  def extractPosts(subreddit: String, jsonContent: String): List[Main.Post] = {
+    implicit val formats = org.json4s.DefaultFormats
+    
+    try {
+      val json = parse(jsonContent)
+      val children = (json \ "data" \ "children").children
+      
+      children.flatMap { child =>
+        try {
+          val title = (child \ "data" \ "title").extract[String]
+          val selftext = (child \ "data" \ "selftext").extract[String]
+          val createdUtc = (child \ "data" \ "created_utc").extract[String]
+          // val createdUtc = (data \ "created_utc").extract[Double].toLong
+          // val date = TextProcessing.formatDateFromUTC(createdUtc)
+          // TODO: implementar formateo estandar de fecha
+          Some((subreddit, title, selftext, createdUtc))
+        } catch {
+          case _: Exception => None
+        }
+      }
+    } catch {
+      case _: Exception => List()
+    }
+  }
+
   // Pure function to download JSON feed from a URL
   def downloadFeed(url: String): String = {
     val source = Source.fromURL(url)
     source.mkString
   }
+
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,21 @@
 object Main {
-  type Subscription = (String, String)
-  type Post = (String, String, String, String)
+  type Subscription = (String, String)          // (subredditName, url)
+  type Post = (String, String, String, String)  // (subreddit, title, selftext, formattedDate)
   def main(args: Array[String]): Unit = {
 
     val subscriptions: List[Subscription] = FileIO.readSubscriptions()
 
-    val allPosts: List[Subscription] = subscriptions.map { subscription =>
-      println(s"Fetching posts from: ${subscription._2}")
-      val posts = FileIO.downloadFeed(subscription._2)
-      (subscription._2, posts)
+    val allPosts: List[Post] = subscriptions.flatMap { subscription =>
+      println(s"Fetching posts from: ${subscription._1} (${subscription._2})")
+      val post = FileIO.downloadFeed(subscription._2)
+      FileIO.extractPosts(subscription._1, post)
     }
 
+    // TODO: a veces algunos caracteres explotan y el formateo se ve mal
     val output = allPosts
-      .map { case (url, posts) => Formatters.formatSubscription(url, posts) }
+      .map { case (subreddit, title, selftext, formattedDate) =>
+        s"Subreddit: $subreddit\n Title: $title\n Date: $formattedDate\n Content: ${selftext.take(100)}...\n" + ("-" * 80)
+      }
       .mkString("\n")
 
     println(output)
