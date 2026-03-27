9d497e9a752485a0982458206836a2304c946643
alejomvides <alejo.vides@mi.unc.edu.ar>
2026-03-27 12:02:05 -0300

    Update: objeto Formatters
    se integró y adapto la nueva estructura de posts

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -1,10 +1,25 @@
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 object Formatters {
 
   // Pure function to format posts from a subscription
-  def formatSubscription(url: String, posts: String): String = {
+  def formatSubscription(url: String, posts: List[Main.Post]): String = {
     val header = s"\n${"=" * 80}\nPosts from: $url \n${"=" * 80}"
-    val formattedPosts = posts.take(80)
-    header + "\n" + formattedPosts
+
+    val formattedPosts = posts.map {
+      case (subreddit, title, selftext, formattedDate) =>
+      s"Subreddit: $subreddit\nTitle: $title\nDate: $formattedDate\nContent: ${selftext.take(80)}...\n${"-"*80}"
+      }.mkString("\n")
+
+      s"$header\n$formattedPosts"
+  }
+  
+  def formatDateFromUTC(utcSeconds: Long): String = {
+    DateTimeFormatter
+      .ofPattern("yyyy-MM-dd HH:mm")
+      .withZone(ZoneId.of("UTC"))
+      .format(Instant.ofEpochSecond(utcSeconds))
   }
 }
