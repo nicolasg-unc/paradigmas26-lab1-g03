3c916838e1d160c06ca3ecd7035318d91afaad20
nahuel-ontivero <nahuel.ontivero@mi.unc.edu.ar>
2026-04-01 14:50:36 -0300

    Se implementó totalScore en Analytics.scala

--- src/main/scala/Analytics.scala
+++ src/main/scala/Analytics.scala
@@ -1,5 +1,5 @@
 object Analytics {
-  // totalScore: List[Post] -> Int
-  // Dada una lista de posts, retorna la suma total de sus scores,
-  // acumulando los valores mediante un fold.
+  def totalScore(xs: List[Main.Post]): Int = {
+    xs.foldLeft(0)((acc, post) => acc + post._5) // post._5 == score
+  }
 }
