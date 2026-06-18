2086d18555f6bfe6de5aee56c3dcb8ad652326e7
nahuel-ontivero <nahuel.ontivero@mi.unc.edu.ar>
2026-06-18 20:43:43 -0300

    feat(ej_2): agregué local_subscriptions.json para mock

--- src/main/scala/Domain.scala
+++ src/main/scala/Domain.scala
@@ -1,5 +1,5 @@
 object Domain {
-  type Subscription = (String, String) // (subredditName, url)
+  type Subscription = (String, String) // (subreddit, url)
   type Post = (String, String, String, String) // (subreddit, title, selftext, formattedDate)
 }
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[Subscription] = FileIO.readSubscriptions("./subscriptions.json")
+    val subscriptions: List[Subscription] = FileIO.readSubscriptions("./local_subscriptions.json")
 
     val allPosts: List[(Subscription, List[Post])] = subscriptions.map { case (subreddit, url) =>
       println(s"Fetching posts from: $url")
