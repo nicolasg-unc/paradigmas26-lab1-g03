e962e6e418f4d681549f2ad4d600ed4fcc0aefa2
nahuel-ontivero <nahuel.ontivero@mi.unc.edu.ar>
2026-06-18 23:52:54 -0300

    feat(ej_6): agregué campo score de tipo Int al tipo Post en Domain.scala

--- src/main/scala/Domain.scala
+++ src/main/scala/Domain.scala
@@ -1,5 +1,5 @@
 object Domain {
   type Subscription = (String, String) // (subreddit, url)
-  type Post = (String, String, String, String) // (subreddit, title, selftext, formattedDate)
+  type Post = (String, String, String, String, Int) // (subreddit, title, selftext, formattedDate, score)
 }
 
